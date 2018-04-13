c1108b588e5626e36430642916b596035b1ab90b
Spencer <spencerjpotts@gmail.com>
2018-04-14 04:57:20 +1000

    Updates package and module documentation.

--- OptionParser/OptionParser/options.py
+++ OptionParser/OptionParser/options.py
@@ -1,3 +1,36 @@
+"""option module documentation
+
+Example:
+    This example demonstrates how to use OptionParser. By importing OptionParser 
+    you are given the ability to set multiple options for the .py file and apply
+    required data types to the option values - parameters::
+
+        import OptionParser
+        OptionParser.options.set_option('arg', str, required=False)
+        print(OptionParser.options.get_options())
+
+
+Attributes:
+    required_options (list): Module level list type variable that hold developers
+        required option(s), and option param data type. The list elements are wrapped in
+        a tuple object for example: [(option, param(s))]
+        
+    option_temp (list): Module level variable
+
+Todo:
+    * set_option(option: str, o_type: type, [required: bool]) add required argument
+    that flags the option as a required option the must be defined or not required.
+    
+    * Create InvalidOption class to handle unknown parsed options from user and provide 
+    appropriate prompts.
+    
+    * add option message suggesting help message --help if help option is enabled.
+    
+    * find better solutions for processing the raw system arguments 
+
+
+"""
+
 import sys
 
 required_options = []
@@ -5,12 +38,51 @@ option_temp = []
     
     
 def set_option(option: str, o_type: type, required: bool) -> bool:
+    """This will append all the function arguments parameters 
+    to the required_options list. Types, Arguments and function return type are
+    Documented in this docstring.
+    
+    Args:
+        option (str): An option set by developer 
+        represented by character or word with '--' notation - prefix. 
+        For example [OPTION: --arg] [VALUE: param] i.e --arg param.
+        
+        o_type (type): The data type needed for the required option. 
+        for example --arg 'param' where param is string type object, and --args 1 2 3 
+        values are int type objects.
+        
+        required (bool): third parameter. TODO
+    Returns:
+        Void
+
+    """
     option_temp.append(option)
     required_options.append((option, o_type))
     
 def get_options():
+    """Example function with args, types, attributes, return types 
+    and raise exceptions documented in the docstring.
+
+    Args:
+        None
+        
+    Attributes:
+        sorted_sys_args (list):  The proposed options and values populated by user.
+        options_list (list): The required options and values list after 
+        discarding unknown options if any.
+        
+        args (list): the system arguments passed through with 
+        file parameter striped away and split with option identifier '--' prefix.
+        
+    Returns:
+        options_list (list): returns all successful required options in a list each item
+        is wrapped in a tuple and each value is wrapped in a list type object
+        i.e list(tuple(str(option), [value])) [(o, [v]), (o, [v])]
     
-    # 
+    Raises:
+        ValueError: If `param2` is equal to `param1`.
+
+    """
     sorted_sys_args = []
     options_list = []
     
--- OptionParser/test.py
+++ OptionParser/test.py
@@ -1,14 +1,27 @@
+"""Example of Python-Tools/OptionParser.
+
+This module demonstrates the usage of OptionParser
+
+Example:
+    Example here demonstrates how to define options using '--' prefix.
+    
+        $ python test.py --arg param --args foo bar
+
+Todo:
+    * Create OptionError Exception class for unknown option being passed.
+
+"""
 import OptionParser
-# if option doesn't exist in parsed arguments raise option error
-OptionParser.options.set_option('keywords', str, required=False)
-OptionParser.options.set_option('index', int, required=False)
-OptionParser.options.set_option('flag', bool, required=False)
-OptionParser.options.set_option('test', str, required=False)
+
+
+OptionParser.options.set_option('arg', str, required=False)
+OptionParser.options.set_option('args', str, required=False)
+
 
 def main(): 
     for k, v in OptionParser.options.get_options():
-        key, value = k, v
-        print(key, ' : ', value)
+        option, values = k, v
+        print(option, ' : ', values)
         
     
 if __name__ == '__main__':
